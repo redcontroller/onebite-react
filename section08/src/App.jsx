484ef0f91110be819daa2ec1ae398ce32099d2c7
redcontroller <jasinis102@gmail.com>
2024-04-15 23:33:50 +0900

    Day-14 강의

---
+++ section08/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import './App.css';
+import Editor from './components/Editor';
+import Header from './components/Header';
+import List from './components/List';
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },    
+  {
+    id: 1,
+    isDone: false,
+    content: "밥먹기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "운동하기",
+    date: new Date().getTime(),
+  },
+];
+
+
+function App() {
+  const [todos, setTodos] = useState(mockData);
+
+  return (
+    <div className='App'>
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  )
+}
+
+export default App
